7bb64bd9616c94c22f588056381a2713812a3cdb
sylvan452 <sylvan452@gmail.com>
2023-09-08 12:19:31 +0000

    test

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -2,7 +2,25 @@
 """ Module of Index views
 """
 from flask import jsonify, abort
-from . import app_views
+from api.v1.views import app_views
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def authorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+        - raise a 401 error
+    """
+    abort(401, description="Unauthorized")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbid() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+        - raise a 403 error
+    """
+    abort(403, description="Forbidden")
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -24,4 +42,3 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
